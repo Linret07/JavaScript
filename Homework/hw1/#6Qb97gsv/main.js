5e5ff6be7d37edf582dd1a2b0bdb79e05bcc6f57
Linret07 <galasirinska@gmail.com>
2025-10-08 22:14:39 +0300

    Зробила домашнє завдання по першій лекції

---
+++ Homework/hw1/#6Qb97gsv/main.js
@@ -0,0 +1,7 @@
+let lastName = `Палієнко`;
+let firstName = `Галина`;
+let middleName = `Володимирівна`;
+let person=lastName + " " + firstName + " " + middleName;
+console.log(person);
+
+
---
+++ Homework/hw1/#ruUtWDUI/main.js
@@ -0,0 +1,8 @@
+let name = prompt("Ваше ім'я?", "Гість");
+alert(`Привіт, ${name}!`);
+let lastName=prompt("Ваше прізвище?", "Гість");
+alert(`Вітаю ${name} ${lastName}`);
+let age=prompt(`Ваш вік?`);
+alert(`Вам ${age} роки!`);
+alert(`Гарного вам дня ${name} ${lastName} ${age} роки!`);
+
