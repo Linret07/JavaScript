75d533724c7208f3abac983239ed809c5dd2c745
Linret07 <galasirinska@gmail.com>
2025-10-19 23:47:26 +0300

    Роблю 3 домашнє завдання

---
+++ Lesson/lesson3/lesson1-3/main.js
@@ -0,0 +1,16 @@
+let users=[
+    {name: 'vasya', age:31, status: false},
+    {name: 'java', age:31, status: false},
+    {name: 'java2', age:31, status: false},
+    {name: 'java3', age:31, status: false},
+    {name: 'java4', age:31, status: false},
+    {name: 'java5', age:31, status: false},
+    {name: 'java6', age:31, status: false},
+    {name: 'java7', age:31, status: false},
+    {name: 'java8', age:31, status: false},
+    {name: 'java9', age:31, status: false},
+]
+for(let i= 0;i < users.length; i++) {
+    document.write(`<h3>user inf - ${users[i].name} </h3>`);
+}
+
