edd3085a73c71e3d99623ae87f1660c98923c2db
Linret07 <galasirinska@gmail.com>
2025-10-21 14:44:37 +0300

    Роблю 3 домашнє завдання

--- Homework/hw3/#4aDbSgh/main.js
+++ Homework/hw3/#4aDbSgh/main.js
@@ -1,23 +1,7 @@
 // — Створити пустий масив. Наповнити його будь-якими значеннями, звертаючись до конкретного індексу. Вивести в консоль
-let empty=[
-    {id:0},
-    {id:1},
-    {id:2},
-    {id:3},
-    {id:4},
-    {id:5}
-]
+let empty=[0,1, 2, 3, 4, 5]
+empty.push(6,7,8,9,10);
+for (let j=0;j<empty.length;j++){
+    console.log(empty[j]);
+}
 
-// for (let j=0;j<empty.length;j++){
-//     console.log(empty[j]);
-// }
-console.log(empty[0]);
-console.log(empty[1]);
-console.log(empty[2]);
-console.log(empty[3]);
-console.log(empty[4]);
-console.log(empty[5]);
-empty.push(empty[6]);
-empty.push(empty[7]);
-console.log(empty[6]);
-console.log(empty[7]);
