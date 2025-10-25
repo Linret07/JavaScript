6d51b8036ce204d75efc5d62688f0bce3d81b086
Linret07 <galasirinska@gmail.com>
2025-10-26 00:50:54 +0300

    Зробила #I2XsG6f

---
+++ Homework/hw4/#I2XsG6f/main.js
@@ -0,0 +1,9 @@
+// – створити функцію, яка обчислює та повертає площу прямокутника зі сторонами  а і б
+function square (aNum,bNum) {
+    const result = aNum * bNum;
+    console.log(result);
+    return result;
+}
+square(5, 10);
+square(7,14);
+
