4154e446738705cd28ee3ff5a6e143f043fee9e8
Linret07 <galasirinska@gmail.com>
2025-10-27 21:56:28 +0200

    Зробила #gEFoxMMO

---
+++ Homework/hw4/#gEFoxMMO/main.js
@@ -0,0 +1,10 @@
+// – створити функцію, яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список (ul li) та виводить його через document.write
+function main(array){
+
+    document.write(`<ul>`);
+    for(const item of array){
+        document.write(`<li>${item}</li>`);
+    }
+    document.write(`</ul>`);
+}
+main([666,96,78,4, 5, `hello`, false]);
