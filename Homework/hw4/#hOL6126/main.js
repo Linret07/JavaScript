10b2f621957379704a9ecbc8b4bb8e2a9b6b8c5a
Linret07 <galasirinska@gmail.com>
2025-10-27 21:36:49 +0200

    Зробила #hOL6126

---
+++ Homework/hw4/#hOL6126/main.js
@@ -0,0 +1,11 @@
+// – створити функцію, яка створює ul з трьома елементами li та виводить його через document.write. Текст li задати через аргумент всім однаковий
+function main() {
+    document.write(`
+<ul>
+<li>Okten</li>
+<li>is</li>
+<li>super</li>
+</ul>
+`)
+}
+main();
