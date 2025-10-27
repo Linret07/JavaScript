d21314a2181d7bd0f0a6a88dc55c92c69ac90f7f
Linret07 <galasirinska@gmail.com>
2025-10-27 21:30:27 +0200

    Зробила #59g0IsA

---
+++ Homework/hw4/#59g0IsA/main.js
@@ -0,0 +1,7 @@
+// – створити функцію, яка створює параграф з текстом та виводить його через document.write. Текст задати через аргумент
+
+function main(text) {
+    document.write(`<p>${text}</p>`);
+}
+main(`Okten`);
+main(`Hello`);
