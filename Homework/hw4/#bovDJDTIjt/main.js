6d86bd1851a8b4540ef1d3f6b443bf7ded3e1de4
Linret07 <galasirinska@gmail.com>
2025-10-28 08:31:19 +0200

    Зробила #bovDJDTIjt

---
+++ Homework/hw4/#bovDJDTIjt/main.js
@@ -0,0 +1,14 @@
+// – створити функцію, яка приймає масив об’єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об’єкту окремий блок.
+
+function main(users){
+    for (const user of users){
+        document.write(`<div>${user.id} ${user.name} ${user.age} </div>`);
+    }
+}
+main([
+    {id: 1, name:`okten`, age:24},
+    {id: 2, name:`luna`, age:24},
+    {id: 3, name:`luna`, age:24},
+    {id: 4, name:`luna`, age:24},
+    {id: 5, name:`luna`, age:24},
+]);
