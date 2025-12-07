947837b5b4e42c8c7c1535ac02b0ddda6e927bce
Linret07 <galasirinska@gmail.com>
2025-12-07 16:44:00 +0200

    Роблю лекцію 8

---
+++ Lesson/lesson8/lesson8-2/main.js
@@ -0,0 +1,51 @@
+// let name = 'vasya';
+// let age =31;
+// let user = {
+//     name,
+//     age,
+//     foo(){
+//         console.log(this);
+//     },
+// }
+// console.log(user);
+// user.foo();
+// let user={
+//     name: 'vasya',
+//     age: 31,
+//     foo(){
+//     console.log('hello');
+//     },
+// };
+// let{name,foo}=user;
+// console.log(name);
+// foo();
+//
+// function asd({name}){
+// console.log(name);
+// }
+// asd({a:'fghth', name:'tyuyui'});
+
+// let {status}=user;
+// console.log(status);
+
+// let name = 'vasya';
+// let age =31;
+// let user = {
+//     name,
+//     age,
+//     foo(){
+//         console.log(this);
+//     },
+//     wife:{
+//         name:'Anna'
+//     }
+// }
+// console.log(user);
+// user.foo();
+//
+// let {name:userName, age:userAge, wife: {wifeName}, wife}=user;
+// console.log(userName, userAge);
+// console.log(wife);
+
+let [a, , c]=[11,22,33];
+console.log([a,c]);    
